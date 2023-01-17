efefc4ac1139ad82b298b66ffc12c47e8877f86e
suzukikoki <114800671+suzukikoki@users.noreply.github.com>
2023-01-17 12:38:16 +0000

    [add]1/17

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,13 +7,15 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "jquery"
+import jQuery from "jquery"
 import "popper.js"
 import "bootstrap"
-
 import '@fortawesome/fontawesome-free/js/all';
 import "../stylesheets/application" 
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+global.$ = jQuery;
+window.$ = jQuery;
